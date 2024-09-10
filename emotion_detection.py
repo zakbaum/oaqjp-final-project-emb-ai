a070e1aa766cdbf26ac5feaa5dbedf4076aa4df9
zak <zachbaum.zb@gmail.com>
2024-09-10 12:56:24 -0400

    task3 update.

--- emotion_detection.py
+++ emotion_detection.py
@@ -14,17 +14,26 @@ def emotion_detector(text_to_analyse):
     formatted_response = json.loads(response.text)
  
      # Extracting requred set of emotions and score from the response
-    emotions = formatted_response['documentSentiment']['emotion']
+    #print(formatted_response[0]['emotion']
+    extracted_emotions = formatted_response['emotionPredictions'][0]['emotion']
+    print(extracted_emotions)
 
-    anger_score = formatted_response['documentSentiment']['anger']
-    disgust_score = formatted_response['documentSentiment']['disgust']
-    fear_score = formatted_response['documentSentiment']['fear']
-    joy_score = formatted_response['documentSentiment']['joy']
-    sadness_score = formatted_response['documentSentiment']['sadness']
+    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+    print(f'this is {anger_score}')
+
+    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
     
+    score_list = [anger_score,disgust_score,fear_score,joy_score,sadness_score]
+    print(score_list)
+    max_score = max(score_list)
+    print(max_score)
 
     # Returning a dictionary containing sentiment analysis results
+    #return("Done.")
     return {'anger': anger_score,'disgust': disgust_score, \
             'fear': fear_score,'joy': joy_score, \
-            'sadness': sadness_score, \ 
-            'dominant_emotion': 'anger'}
+            'sadness': sadness_score, \
+            'dominant_emotion': max_score}
