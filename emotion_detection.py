4120dbe5a9bf2cd3ded01213b54139ace32c4f47
zak <zachbaum.zb@gmail.com>
2024-09-10 09:31:50 -0400

    updates to task3.

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,18 +1,12 @@
 
 # Import necessary libraries from Flask
-from flask import Flask, redirect, request, render_template, url_for
+import requests
 
-# Instantiate Flask application
-app = Flask(__name__)
-
-text_to_analyze = 
-
-URL: 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-Headers: {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-Input json: { "raw_document": { "text": text_to_analyse } }
-
-@app.route("/")
-def emotion_detector("text_to_analyse"):
-    return "text"
-
-
+def emotion_detector(text_to_analyse):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    e_obj = { "raw_document": { "text": text_to_analyse } }  # Create a dictionary with the text to be analyzed
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}  # Set the headers required for the API request
+    response = requests.post(url, json = e_obj, headers=header)  # Send a POST request to the API with the text and headers
+    return response.text  # Return the response text from the API
+    
+ 
