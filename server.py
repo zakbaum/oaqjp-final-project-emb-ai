59b8c391f7ac7940f8f964583e1e69a70528138b
zak <zachbaum.zb@gmail.com>
2024-09-10 17:14:19 -0400

    task8

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+"""My Emotion Dectector app"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,21 +6,23 @@ app = Flask("Emotion Detection")
 
 @app.route("/")
 def render_index_page():
+    """html renderer"""
     return render_template('index.html')
 
 @app.route("/emotionDetector")
 def emo_detection():
+    """emo detector"""
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
 
     # Pass the text to the sentiment_analyzer function and store the response
     response = emotion_detector(text_to_analyze)
- 
+
     if response['dominant_emotion'] == 'None':
         return  "Invalid text! Please try again!."
-    else:
-       # Return the emotion detected from given string
-       return (f'For the given statement, the system response is {response}')
+
+    # Return the emotion detected from given string
+    return f'For the given statement, the system response is {response}'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
